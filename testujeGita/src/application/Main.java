2d4cf78e243aef0b34cb7775524caff64eaad67d
pawcio <pawczinx@gmail.com>
2016-05-28 16:26:21 +0200

    Dodany button

--- testujeGita/src/application/Main.java
+++ testujeGita/src/application/Main.java
@@ -3,15 +3,18 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			StackPane stackPane = new StackPane();
+			Button button = new Button("DZIALA");
+			stackPane.getChildren().add(button);
+			Scene scene = new Scene(stackPane);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
